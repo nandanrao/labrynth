ac252fcb9d1de952f295913e6cb4d9e834d7fda7
Nandan Rao <nandanmarkrao@gmail.com>
2015-01-18 19:01:31 -0500

    first commit

--- js/components/room.directive.js
+++ js/components/room.directive.js
@@ -1,20 +1,4 @@
 angular.module('maintenenceWorker')
-	.directive('door', function (){
-		return {
-			restrict: 'E',
-			replace: true,
-			templateUrl: 'components/door.html',
-			link: function (scope, el, attrs){
-				var table = {
-					north: { x:41, y:0, w: 25, h: 7 },
-					east: { x:100, y:41, w: 7, h: 25 },
-					south: { x:41, y:100, w: 25, h: 7 },
-					west: { x:0, y:41, w: 7, h: 25}
-				}
-				_.merge(scope, table[attrs.direction])
-			}
-		}
-	})
 	.directive('room', function (){
 		return {
 			restrict: 'E',
